4a5c17c54e44cc18252c2010c311492fa7c27a13
Sven Kobow <sven.kobow@diva-e.com>
2020-02-22 21:20:50 +0100

    Add extension method for mapping Image to ImageDto

--- Backend/mapping/src/main/kotlin/com/mhp/coding/challenges/mapping/models/dto/ImageDto.kt
+++ Backend/mapping/src/main/kotlin/com/mhp/coding/challenges/mapping/models/dto/ImageDto.kt
@@ -1,5 +1,8 @@
 package com.mhp.coding.challenges.mapping.models.dto
 
+import com.mhp.coding.challenges.mapping.models.db.Image
 import com.mhp.coding.challenges.mapping.models.db.ImageSize
 
 data class ImageDto(val id: Long, val url: String, val imageSize: ImageSize)
+
+fun Image.toImageDto() = ImageDto(this.id, this.url, this.imageSize)
