03f72ed2131a8ec13e87921f490513d2a7f562e8
Sven Kobow <sven.kobow@diva-e.com>
2020-02-22 21:44:05 +0100

    Refactoring of ArticleBlockMapper to simplify code

--- Backend/mapping/src/main/kotlin/com/mhp/coding/challenges/mapping/mappers/blocks/ArticleBlockMapper.kt
+++ Backend/mapping/src/main/kotlin/com/mhp/coding/challenges/mapping/mappers/blocks/ArticleBlockMapper.kt
@@ -4,11 +4,9 @@ import com.mhp.coding.challenges.mapping.models.db.blocks.ArticleBlock
 import com.mhp.coding.challenges.mapping.models.dto.blocks.ArticleBlockDto
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
-import java.util.Objects
-import java.util.stream.Collectors
 
 @Component
-class ArticleBlockMapper(private val strategies: List<ArticleBlockMappingStrategy>) {
+class ArticleBlockMapper(private val strategies: List<ArticleBlockMappingStrategy<*>>) {
 
     companion object {
         @Suppress("JAVA_CLASS_ON_COMPANION")
@@ -16,25 +14,16 @@ class ArticleBlockMapper(private val strategies: List<ArticleBlockMappingStrateg
         private val log = LoggerFactory.getLogger(javaClass.enclosingClass)
     }
 
-    fun mapAll(blocks: Set<ArticleBlock>): Set<ArticleBlockDto?> {
-        return blocks.stream()
-                .map(this::map)
-                .filter(Objects::nonNull)
-                .collect(Collectors.toSet())
-    }
+    fun mapAll(blocks: Set<ArticleBlock>): List<ArticleBlockDto> = blocks.mapNotNull(this::map)
 
-    fun map(block: ArticleBlock): ArticleBlockDto? {
-        val strategy = getStrategyFor(block::class.java)
-        if (strategy == null) {
-            log.warn("No mapper found for block type: [${block::class.java.name}]")
-            return null
-        }
-        return strategy.map(block)
-    }
+    fun map(block: ArticleBlock): ArticleBlockDto? =
+            getMappingStrategyFor(block)?.map(block) ?: run {
+                log.warn("No mapper found for block type: [${block::class.java.name}]")
+                null
+            }
 
-    private fun getStrategyFor(blockType: Class<*>): ArticleBlockMappingStrategy? {
-        return strategies.firstOrNull {
-            it.canMap(blockType)
-        }
-    }
+    private fun getMappingStrategyFor(block: ArticleBlock): ArticleBlockMappingStrategy<*>? =
+            strategies.firstOrNull {
+                it.canMap(block)
+            }
 }
